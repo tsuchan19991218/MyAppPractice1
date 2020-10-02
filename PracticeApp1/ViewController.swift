81b1fb68011792191bc19fe7679993268a45ee85
古賀　吏 <ylwrvr.t.kogagmail.com@MacBook-Pro.local>
2020-10-03 02:26:01 +0900

    initial commit

---
+++ PracticeApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeApp1
+//
+//  Created by 古賀　吏 on 2020/10/03.
+//  Copyright © 2020 古賀　吏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PracticeApp1Tests/PracticeApp1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PracticeApp1Tests.swift
+//  PracticeApp1Tests
+//
+//  Created by 古賀　吏 on 2020/10/03.
+//  Copyright © 2020 古賀　吏. All rights reserved.
+//
+
+import XCTest
+@testable import PracticeApp1
+
+class PracticeApp1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
